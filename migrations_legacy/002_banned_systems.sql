cc7f778c11f64fea31c1c75d0d042f2a4f778160
Starshine System <us@starshines.xyz>
2021-09-26 19:55:58 +0200

    remove migrations_legacy directory

--- migrations_legacy/002_banned_systems.sql
+++
@@ -1,5 +0,0 @@
-alter table guilds add column banned_systems char(5)[] not null default array[]::char(5)[];
-
----- create above / drop below ----
-
-alter table guilds drop column banned_systems;
--- migrations_legacy/005_redirects.sql
+++
@@ -1,5 +0,0 @@
-alter table guilds add column redirects json not null default '{}';
-
----- create above / drop below ----
-
-alter table guilds drop column redirects;
